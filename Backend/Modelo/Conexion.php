a522012993aae183371bdf6807a0078798777f66
jason79354656 <jasonchoquecananviri@gmail.com>
2019-09-01 12:14:57 -0400

    conexion ala base de datos as

--- Backend/Modelo/Conexion.php
+++ Backend/Modelo/Conexion.php
@@ -5,13 +5,13 @@ Class Conexion {
     
 public static function conexcion(){    
     try{
-    $pdo_conexion = new PDO('mysql:host=localhost;dbname=.USER', USER,PASS); 
+    $pdo_conexion = new PDO('mysql:host=localhost;dbname=.USER',USER,PAS); 
     $pdo_conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXEPTION);
     $pdo_conexion->exec("SET CHARACTER SET UTF8");
     }catch(Exception $e) {
         die('Error' .$e->getMessage());
         echo "Linea del error".$e->getLIne();
-        echo "";
+        
         }
         return $pdo_conexion;
     }
