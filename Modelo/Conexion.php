6b2b185ff6fc2bb7362a2980307ff8639b76362a
jason79354656 <jasonchoquecananviri@gmail.com>
2019-09-01 12:01:04 -0400

    coneccion a la base de datos

---
+++ Modelo/Conexion.php
@@ -0,0 +1,20 @@
+<?php 
+
+require("config.php"); 
+Class Conexion {
+    
+public static function conexcion(){    
+    try{
+    $pdo_conexion = new PDO('mysql:host=localhost;dbname=.USER', USER,PASS); 
+    $pdo_conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXEPTION);
+    $pdo_conexion->exec("SET CHARACTER SET UTF8");
+    }catch(Exception $e) {
+        die('Error' .$e->getMessage());
+        echo "Linea del error".$e->getLIne();
+
+        }
+        return $pdo_conexion;
+    }
+    
+}
+?>
